15219e29b03509e5d3539cc5c7e5ad7d91a8c78c
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-21 06:56:51 -0600

    list with posts working

---
+++ src/componentes/Post.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+class Post extends Component {
+    render() {
+        const {id, title} = this.props.info;
+        return (
+            <tr>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>
+                    <Link to={`/post/${id}`} className="btn btn-primary">Ver</Link>
+                    <button type="button" className="btn btn-danger">Borrar</button>
+                </td>
+            </tr>
+        )
+    }
+}
+
+export default Post;
--- src/componentes/Router.js
+++ src/componentes/Router.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter, route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 import Header from './Header';
 import Navegacion from './Navegacion';
+import Posts from './Posts';
 
 class Router extends Component {
     state = {
@@ -28,6 +29,17 @@ class Router extends Component {
                     <div className="row justify-content-center">
                         <Header />
                         <Navegacion />
+                        <Switch>
+                            <Route exact path="/" render={ () => {
+                                return (
+                                    <Posts 
+                                        posts={this.state.posts}
+                                    />
+                                )
+                            }}
+                            
+                            />
+                        </Switch>
                     </div>
                 </div>
             </BrowserRouter>
